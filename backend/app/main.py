5898f18924a27159703b0ba5c6b5b059a3f0d670
Kiah <kiahdmandaliya@gmail.com>
2026-03-07 12:28:30 +0530

    updated gitignore

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,   # or ["*"] for development
+    allow_origins=origins,   
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
